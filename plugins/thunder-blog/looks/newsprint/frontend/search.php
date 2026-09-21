204d85d99ed3f14f5a22963c3b4e18a14b3ae924
Eathorne2 <deepxchannel@gmail.com>
2026-09-21 12:33:28 +0200

    adjustments

--- plugins/thunder-blog/looks/newsprint/frontend/search.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace ThunderBlog;
-
-require __DIR__ . '/index.php';
-do_action('thunder_blog_after_search', ['query' => $search_query ?? '', 'posts' => $posts ?? [], 'pagination' => $pagination ?? []]);
